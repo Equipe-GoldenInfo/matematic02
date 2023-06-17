d91ee43e9fcb350b0e995db8455da93c0c418a60
RyanVictorDev <vryan8294@gmail.com>
2023-06-16 23:35:08 -0300

    Atualização031606

--- assets/js/massa.js
+++
@@ -1,9 +0,0 @@
-function calculate() {
-    var mass = parseFloat(document.getElementById("inputMass").value);
-    var volume = parseFloat(document.getElementById("inputVolume").value);
-    var resultDensity = document.getElementById("resultDensity");
-  
-    var density = mass / volume;
-    resultDensity.textContent = density.toFixed(2);
-  }
-  
