6f6376750b74c69f7d0787736556ba2a261b89ca
RyanVictorDev <vryan8294@gmail.com>
2023-06-18 12:32:56 -0300

    atualização0118062023

--- assets/js/peso.js
+++ assets/js/peso.js
@@ -1,12 +1,35 @@
-function convertWeight() {
-    var inputWeight = parseFloat(document.getElementById("inputWeight").value);
-    var outputPounds = document.getElementById("outputPounds");
-    var outputOunces = document.getElementById("outputOunces");
-  
-    var pounds = inputWeight * 2.20462;
-    var ounces = inputWeight * 35.274;
-  
-    outputPounds.value = pounds.toFixed(2);
-    outputOunces.value = ounces.toFixed(2);
-  }
-  
+function converterParaOncas() {
+  var quilos = parseFloat(document.getElementById("peso-quilos").value);
+  var oncas = (quilos * 35.27396);
+  document.getElementById("peso-oncas").value = oncas.toFixed(2);
+
+  or
+
+  var libras = parseFloat(document.getElementById("peso-libras").value);
+  var oncas = (libras*16);
+  document.getElementById("peso-oncas").value = oncas.toFixed(2);
+}
+
+function converterParaQuilos() {
+  var oncas = parseFloat(document.getElementById("peso-oncas").value);
+  var quilos = (oncas / 35.27396);
+  document.getElementById("peso-quilos").value = quilos.toFixed(2);
+
+  or
+
+  var libras = parseFloat(document.getElementById("peso-libras").value);
+  var quilos = (libras/2.205);
+  document.getElementById("peso-quilos").value = quilos.toFixed(2);
+}
+
+function converterParaLibras() {
+  var quilos = parseFloat(document.getElementById("peso-quilos").value);
+  var libras = (quilos*2.205);
+  document.getElementById("peso-libras").value = libras.toFixed(2);
+
+  or
+
+  var oncas = parseFloat(document.getElementById("peso-oncas").value);
+  var libras = (oncas/16);
+  document.getElementById("peso-libras").value = libras.toFixed(2);
+}
